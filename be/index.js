ff32c78aa72cc7761c4f8f000b9336d1d42332b8
fineaszirbo <fineas.zirbo.zmc@gmail.com>
2022-03-25 01:28:49 +0200

    first

---
+++ be/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express ();
+const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "coinpass",
+});
+
+app.post("/register",(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    db.query("INSERT INTO coinpass.users (username, password) VALUES (?,?)",
+    [username, password],
+    (err,result)=>{
+      console.log(err);
+    })
+});
+
+app.post("/login",(req,res)=> {
+    const username = req.body.username;
+    const password = req.body.password;
+    db.query(
+        "SELECT * FROM coinpass.users WHERE username = ? AND password = ?",
+        [username, password],
+        (err,result) => { 
+          if (err) {
+              res.send({err: err});
+          }
+          if(result.length > 0){
+              res.send(result);
+          } else {
+              res.send({message:"wrong username/password"});
+          }
+    });
+});
+
+
+
+app.listen(3001,()=>{
+    console.log("running on port 3001")
+});
