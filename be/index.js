883b1e931abdb9f1456a87a260021deb794a45a2
fineaszirbo <fineas.zirbo.zmc@gmail.com>
2022-03-25 10:42:17 +0200

    removed be CORS

--- be/index.js
+++ be/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express ();
 const mysql = require('mysql');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 
-app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
