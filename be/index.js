19b1d9fb3b79e1db1c7922fa12a9681ce36fb010
fineaszirbo <fineas.zirbo.zmc@gmail.com>
2022-03-26 15:00:25 +0200

    run command

--- be/index.js
+++ be/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express ();
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
+//npm run devStart
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
